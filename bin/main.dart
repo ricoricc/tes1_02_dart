79b4cc507e6baaf658029820d14eb7bab48a2605
ricoricc <rico24.phone@gmail.com>
2022-03-16 14:44:13 +0700

    design class

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,18 @@
+import 'dart:io';
+import 'fproduct.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  int inp = 0;
+
+  int num = 0;
+  while (inp < 5) {
+    print("1. Menambahkan Barang Baru");
+    print("2. Menampilkan Sebuah Barang");
+    print("3. Meletakkan Barang pada lokasi tertentu");
+    print("4. Menampilkan seluruh barang pada lokasi tertentu");
+    print("5. Exit");
+    stdout.write("Pilih Opsi Menu: ");
+    String? iData = stdin.readLineSync();
+    inp = int.parse(iData!);
+  }
 }
